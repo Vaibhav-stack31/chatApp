0a1290d6141f8db7c755d326a58f475419f0735d
Vaibhav-stack31 <tvaibhav23it@student.mes.ac.in>
2025-04-26 12:17:10 +0530

    Update utils.js

--- server/src/lib/utils.js
+++ server/src/lib/utils.js
@@ -1,15 +1,16 @@
-import jwt from 'jsonwebtoken'
+import jwt from 'jsonwebtoken';
 
 export const generateToken = (userID, res) => {
     const token = jwt.sign({ userID }, process.env.JWT_SECRET, {
         expiresIn: "7d"
-    })
-    res.cookie("jwt", token,{
-        maxAge: 7 * 24 * 60 * 60 * 100, //milliseconds
+    });
+
+    res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days in ms
         httpOnly: true,
-        sameSite: "strict",
-        secure: process.env.NODE_ENV !== "development"
+        secure: true,               // Required for HTTPS (Render uses HTTPS)
+        sameSite: "None"            // Required for cross-origin cookies
     });
 
     return token;
-}
+};
