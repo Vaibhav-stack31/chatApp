450a8d120db0b1f8fe5c6706bd1d14a989c435fb
Vaibhav-stack31 <tvaibhav23it@student.mes.ac.in>
2025-04-26 12:21:47 +0530

    Update useAuthStore.js

--- client/src/store/useAuthStore.js
+++ client/src/store/useAuthStore.js
@@ -47,6 +47,7 @@ export const useAuthStore = create((set, get) => ({
             set({ authUser: res.data });
             toast.success("Logged in successfully");
             get().connectSocket();
+            window.location.reload();
         } catch (error) {
             toast.error(error.response.data.message);
         } finally {
