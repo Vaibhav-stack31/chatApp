aa2f4d22b40b94d2654c3f00b27c6e8004731639
Vaibhav-stack31 <tvaibhav23it@student.mes.ac.in>
2025-04-26 11:34:35 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -15,7 +15,7 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.use(cookieParser());
 app.use(cors({
-    origin: "hhttps://chatapp-frontend-2fy8.onrender.com",
+    origin: "https://chatapp-frontend-2fy8.onrender.com",
     credentials: true,
 }));
 app.use('/api/auth', authRoutes);
